b183ea58e30f1aa4eb21c3b61e5d1e9a4ae5d7ef
Quang Trong <quangtrong1506@gmail.com>
2023-12-02 07:26:59 +0700

    fix loading

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let time = 0;
 let currentId = null;
 let isJustStartedPlayingVideo = true;
+let isLoading = false;
 const typeOfAdvertisement = {
     beforePlayingTheVideo: Symbol('before_playing_the_video'),
     middleOfTheVideo: Symbol('middle_of_the_video'),
@@ -21,24 +22,31 @@ const handleAdvertisement = (type) => {
     console.log('Ads: ', type);
     switch (type) {
         case typeOfAdvertisement.beforePlayingTheVideo:
-            if (currentId) window.location = `https://youtu.be/${currentId}`;
+            let id = window.location.href.split('?v=')[1]?.split('&')[0];
+            if (id && !isLoading) {
+                window.location = `https://youtu.be/${id}`;
+                isLoading = true;
+            }
             break;
 
         case typeOfAdvertisement.middleOfTheVideo:
-            if (currentId) window.location = `https://youtu.be/${currentId}&t=${time}s`;
+            if (currentId && !isLoading) {
+                window.location = `https://youtu.be/${currentId}?t=${time}`;
+                isLoading = true;
+            }
             break;
 
         case typeOfAdvertisement.adsCanBeSkipped:
             document.querySelector('.ytp-ad-skip-button-modern.ytp-button')?.click();
             break;
 
-        default: {
+        default:
             console.log('Ads undefined!');
             break;
-        }
     }
 };
 navigation.addEventListener('navigate', () => {
+    isLoading = false;
     let id = window.location.href.split('?v=')[1]?.split('&')[0];
     if (id) {
         currentId = id;
