77acb0890b88c8b50c6eaee87645a39db83bcba1
Quang Trong <quangtrong1506@gmail.com>
2023-12-29 21:46:19 +0700

    fix  v1.0.4

--- js/main.js
+++ js/main.js
@@ -1,70 +1,80 @@
-let time = 0;
-let currentId = null;
-let isJustStartedPlayingVideo = true;
-let isLoading = false;
-const typeOfAdvertisement = {
-    beforePlayingTheVideo: Symbol('before_playing_the_video'),
-    middleOfTheVideo: Symbol('middle_of_the_video'),
-    adsCanBeSkipped: Symbol('ads_can_be_skipped'),
-    nextVideo: Symbol('next_video'),
-    isPlayingAccordingToTheList: Symbol('is_playing_according_to_the_list'),
+const state = {
+    time: 0, //? Thời gian video đang phát
+    currentId: null, //? id video đang phát
+    isHandleAds: false, //? Đang xử lý quảng cáo
+    isPlayingAccordingToTheList: false, //? có phát dướu dạng danh sách không
 };
+const typeOfAdvertisement = {
+    shortAds: Symbol('short_ad'), // Quảng cáo ngắn hoặc k thể bỏ qua
+    adsCanBeSkipped: Symbol('ads_can_be_skipped'), // có thể next
+    nextVideo: Symbol('next_video'), // next video
+};
+
+const setDataOnload = () => {
+    state.isHandleAds = false;
+    let id = window.location.href.split('?v=')[1]?.split('&')[0];
+    let t = window.location.href.split('t=')[1]?.split('s')[0];
+    if (
+        document.querySelectorAll(
+            '.playlist-items.style-scope.ytd-playlist-panel-renderer .ytd-playlist-panel-renderer'
+        ).length > 0
+    )
+        state.isPlayingAccordingToTheList = true;
+    if (id) {
+        state.currentId = id;
+        state.time = !isNaN(parseInt(t)) ? parseInt(t) : 0;
+    } else {
+        setTimeout(setDataOnload, 100);
+    }
+};
+window.onload = () => {
+    setDataOnload();
+    navigation.addEventListener('navigate', () => {
+        setDataOnload();
+    });
+};
+// Todo: Check ads
 setInterval(() => {
+    if (state.isHandleAds || fs_status()) return;
     if (document.querySelector('.ytp-ad-skip-button-modern.ytp-button'))
         handleAdvertisement(typeOfAdvertisement.adsCanBeSkipped);
-    else if (document.querySelector('.ytp-ad-text.ytp-ad-preview-text-modern')) {
-        if (isJustStartedPlayingVideo)
-            handleAdvertisement(typeOfAdvertisement.beforePlayingTheVideo);
-        else handleAdvertisement(typeOfAdvertisement.middleOfTheVideo);
+    // handleAdvertisement(typeOfAdvertisement.shortAds);
+    else if (
+        document.querySelector('.ytp-ad-text.ytp-ad-preview-text-modern') &&
+        !state.isPlayingAccordingToTheList
+    ) {
+        handleAdvertisement(typeOfAdvertisement.shortAds);
     } else if (
         document.querySelector(
             '.ytp-autonav-endscreen-upnext-header .ytp-autonav-endscreen-upnext-header-countdown-number'
         )
     ) {
         handleAdvertisement(typeOfAdvertisement.nextVideo);
-    } else if (!isLoading) {
-        time = Math.floor(document.querySelector('video')?.currentTime) || 0;
+    } else if (!state.isHandleAds) {
+        let t = Math.floor(document.querySelector('video')?.currentTime) || 0;
+        state.time = t > 10 ? t : 0;
     }
 }, 100);
 const handleAdvertisement = (type) => {
     console.log('Ads: ', type);
     switch (type) {
-        case typeOfAdvertisement.beforePlayingTheVideo:
+        //Todo: xử lý bỏ qua ads (cái không thể next)
+        case typeOfAdvertisement.shortAds:
             if (
-                document.querySelectorAll(
-                    '.playlist-items.style-scope.ytd-playlist-panel-renderer .ytd-playlist-panel-renderer'
-                ).length > 0
+                state.currentId &&
+                !state.isHandleAds &&
+                !state.isPlayingAccordingToTheList &&
+                !fs_status()
             ) {
-                handleAdvertisement(typeOfAdvertisement.isPlayingAccordingToTheList);
-                break;
-            }
-            let id = window.location.href.split('?v=')[1]?.split('&')[0];
-            if (id && !isLoading) {
-                localStorage.setItem('time_ext_skip-first', ' date: ' + new Date());
-                window.location = `https://youtu.be/${id}`;
-                isLoading = true;
+                window.location = `https://youtu.be/${state.currentId}?t=${state.time}`;
+                state.isHandleAds = true;
             }
             break;
-
-        case typeOfAdvertisement.middleOfTheVideo:
-            if (
-                document.querySelectorAll(
-                    '.playlist-items.style-scope.ytd-playlist-panel-renderer .ytd-playlist-panel-renderer'
-                ).length > 0
-            ) {
-                handleAdvertisement(typeOfAdvertisement.isPlayingAccordingToTheList);
-                break;
-            }
-            if (currentId && !isLoading) {
-                localStorage.setItem('time_ext_skip', 'time: ' + time + ' - date: ' + new Date());
-                window.location = `https://youtu.be/${currentId}?t=${time}`;
-                isLoading = true;
-            }
-            break;
-
+        //Todo: next video nếu có nút next
         case typeOfAdvertisement.adsCanBeSkipped:
             document.querySelector('.ytp-ad-skip-button-modern.ytp-button')?.click();
             break;
+        //Todo: hết video next luôn
         case typeOfAdvertisement.nextVideo:
             if (
                 document.querySelectorAll(
@@ -80,42 +90,17 @@ const handleAdvertisement = (type) => {
                 )
                 ?.click();
             break;
-        case typeOfAdvertisement.isPlayingAccordingToTheList:
-            const showTime = (time) => {
-                console.log(`Skip ads after ${time} seconds`);
-                isLoading = true;
-                setTimeout(() => {
-                    isLoading = false;
-                    console.log('Ads have finished playing');
-                }, time * 1000);
-            };
-            let t =
-                Math.floor(document.querySelector('video')?.duration || 0) -
-                (Math.floor(document.querySelector('video')?.currentTime) || 0);
-
-            if (t > 0 && !isLoading) showTime(t);
-            break;
         default:
             console.log('Ads undefined!');
             break;
     }
 };
-const setDataOnload = () => {
-    isLoading = false;
-    let id = window.location.href.split('?v=')[1]?.split('&')[0];
-    let t = window.location.href.split('?v=')[1]?.split('&')[1];
-    if (id) {
-        currentId = id;
-        isJustStartedPlayingVideo = true;
-        setTimeout(() => {
-            isJustStartedPlayingVideo = false;
-        }, 5000);
-        time = 0;
-    }
-};
-window.onload = () => {
-    setDataOnload();
-    navigation.addEventListener('navigate', () => {
-        setDataOnload();
-    });
+const fs_status = () => {
+    if (
+        document.fullscreenElement ||
+        document.webkitFullscreenElement ||
+        document.mozFullScreenElement
+    )
+        return true;
+    return false;
 };
